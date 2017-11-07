0fc8b4bd0ad562f5af8cae63a817fb983f6f1b07
chlaudiah <chlaudiahjulinarsl@gmail.com>
2017-11-08 00:10:05 +0700

    Update LoginContext.java

--- kelompokbengkel14/src/test/java/id/sparepart/bengkel/kelompokbengkel14/test/LoginContext.java
+++ kelompokbengkel14/src/test/java/id/sparepart/bengkel/kelompokbengkel14/test/LoginContext.java
@@ -14,34 +14,45 @@ import junit.framework.Assert;
  * @author BegalPapua
  */
 public class LoginContext {
-    String respon;
-
-    @Dengan("^Data user baru yang akan dimasukan tersedia dan tidak duplikat dengan data yang telah ada di sistem$")
-    public void data_user_baru_yang_akan_dimasukan_tersedia_dan_tidak_duplikat_dengan_data_yang_telah_ada_di_sistem() throws Throwable {
+    @Dengan("^Data admin yang dimasukkan dan data admin tersedia dalam data yang telah ada di sistem$")
+    public void data_admin_yang_dimasukkan_dan_data_admin_tersedia_dalam_data_yang_telah_ada_di_sistem() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        boolean expected = false;
-        ///kode untuk cek ke database apakah user sudah ada
-        boolean result = false;///harusnya dari database.
-        Assert.assertEquals(expected, result);
-
+        //throw new PendingException();
     }
 
-    @Ketika("^Memasukan data user dengan nama \"([^\"]*)\"$")
-    public void memasukan_data_user_dengan_nama(String arg1) throws Throwable {
+    @Ketika("^Memasukkan data admin dengan \"([^\"]*)\" dan \"([^\"]*)\"$")
+    public void memasukkan_data_admin_dengan_dan(String arg1, String arg2) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        //respon =Demo.addUser(arg1);
+        //throw new PendingException();
     }
 
-    @Maka("^respon \"([^\"]*)\"$")
-    public void respon(String arg1) throws Throwable {
+    @Maka("^respon sukses dengan pesan \"([^\"]*)\"$")
+    public void respon_sukses_dengan_pesan(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        ///kode untuk cek ke database apakah user sudah ada
-        Assert.assertEquals(arg1, respon);
+        //throw new PendingException();
+    }
+
+    @Dengan("^Data admin yang dimasukkan dan username admin tidak tersedia dalam data yang telah ada di sistem$")
+    public void data_admin_yang_dimasukkan_dan_username_admin_tidak_tersedia_dalam_data_yang_telah_ada_di_sistem() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
     }
 
     @Maka("^respon gagal dengan pesan \"([^\"]*)\"$")
     public void respon_gagal_dengan_pesan(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        Assert.assertEquals(arg1, respon);
+        //throw new PendingException();
+    }
+
+    @Dengan("^Data admin yang dimasukkan dan password admin tidak tersedia dalam data yang telah ada di sistem$")
+    public void data_admin_yang_dimasukkan_dan_password_admin_tidak_tersedia_dalam_data_yang_telah_ada_di_sistem() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+    }
+
+    @Dengan("^data admin yang dimasukkan dan data admin tidak tersedia dalam data yang telah ada di sistem$")
+    public void data_admin_yang_dimasukkan_dan_data_admin_tidak_tersedia_dalam_data_yang_telah_ada_di_sistem() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
     }
 }
