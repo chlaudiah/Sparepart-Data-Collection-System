b0ea08faafd96212fee25dd7f1c9c489349d37bd
septianaputri <septianaputri131@gmail.com>
2017-11-13 09:52:15 +0700

    Add files via upload

---
+++ kelompokbengkel14/src/test/java/id/sparepart/bengkel/kelompokbengkel14/test/CekStock/CekStockTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.sparepart.bengkel.kelompokbengkel14.CekStock;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author MSI
+ */
+@RunWith (Cucumber.class)
+@CucumberOptions(plugin = { "pretty", "html:target/cucumber","json:target/cucumber.json" })
+public class CekStockTest {
+    
+}
