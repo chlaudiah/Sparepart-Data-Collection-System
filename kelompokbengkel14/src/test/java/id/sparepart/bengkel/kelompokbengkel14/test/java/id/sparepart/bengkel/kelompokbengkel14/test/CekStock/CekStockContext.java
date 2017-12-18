5d870c498a7648d48bf57eb4656f0cbd82efe9bb
chlaudiah <chlaudiahjulinarsl@gmail.com>
2017-12-18 13:19:43 +0700

    Update CekStockContext.java

--- kelompokbengkel14/src/test/java/id/sparepart/bengkel/kelompokbengkel14/test/java/id/sparepart/bengkel/kelompokbengkel14/test/CekStock/CekStockContext.java
+++ kelompokbengkel14/src/test/java/id/sparepart/bengkel/kelompokbengkel14/test/java/id/sparepart/bengkel/kelompokbengkel14/test/CekStock/CekStockContext.java
@@ -18,29 +18,36 @@ public class CekStockContext {
     public void admin_menginputkan_dan_dilakukan_pengecekan_tersebut_pada_di_sistem(String arg1, String arg2, String arg3) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new PendingException();
+        int expected = App.Aplikasi.getIdBarang(b.getId_barang());
+        int result = App.Aplikasi.getIdBarang(b.getId_barang());
+        Assert.assertEquals(expected,result);
     }
 
     @Ketika("^\"([^\"]*)\" ada maka akan ditampilkan \"([^\"]*)\" yang memuat \"([^\"]*)\", \"([^\"]*)\" dan \"([^\"]*)\" sparepart yang tersedia$")
     public void ada_maka_akan_ditampilkan_yang_memuat_dan_sparepart_yang_tersedia(String arg1, String arg2, String arg3, String arg4, String arg5) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new PendingException();
+        respon = App.Aplikasi.DataBarang(b.getId_barang());
     }
 
     @Maka("^respon sukses dengan menampilkan data \"([^\"]*)\" yang dicari$")
     public void respon_sukses_dengan_menampilkan_data_yang_dicari(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new PendingException();
+        Assert.assertEquals(respon, "Data tersedia");
     }
 
     @Ketika("^\"([^\"]*)\" yang dicari tidak ada$")
     public void yang_dicari_tidak_ada(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new PendingException();
+        respon = App.Aplikasi.DataBarang(0);
     }
 
     @Maka("^respon gagal dengan menampilkan pesan \"([^\"]*)\"$")
     public void respon_gagal_dengan_menampilkan_pesan(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new PendingException();
-        }
+        Assert.assertEquals(respon, "Data tidak tersedia");
+      }
 }
